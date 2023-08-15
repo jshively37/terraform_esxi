fe43e792c64cabe0b7fd89221584fd8d87d5175b
Jeffrey <jeffrey.shively@hashicorp.com>
2023-08-15 08:25:27 -0400

    removing static username and password.

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "vsphere" {
   vsphere_server       = var.esxi_host
-  user                 = var.esxi_username
-  password             = var.esxi_password
   allow_unverified_ssl = var.esxi_allow_unverified_ssl
 }
 
@@ -22,9 +20,9 @@ data "vsphere_network" "network" {
 data "vsphere_resource_pool" "pool" {}
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_name
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  name                       = var.vm_name
+  resource_pool_id           = data.vsphere_resource_pool.pool.id
+  datastore_id               = data.vsphere_datastore.datastore.id
   wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout  = 0
 
